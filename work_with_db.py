7b7b2b839167c3b472744b545730fefb6024f593
Ivan Kaurov <crazyanimalcz@gamil.com>
2025-02-24 19:55:23 +0300

    Initial commit

---
+++ work_with_db.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+
+def work_with_db(db_name: str) -> None:
+    with psycopg2.connect(dbname=db_name) as conn:
+        with conn.cursir() as cur:
+            cur.execute("""
+            CREATE TABLE vacancies(
+            vacancy_id INT PRIMARY KEY,
+            vacancy_name VARCHAR NOT NULL,
+            vacancy_salary INT,
+            vacancy_url TEXT,
+            employ_id INT REFERENCES employees(employee_id))
+            """)
+        conn.commit()
+        conn.close()
+
